dbfc0c2e7e2b58c84b20f9a0f05c1b41fdb13f6d
Albin Xavier Varghese <154511248+albinxavier1@users.noreply.github.com>
2024-05-01 22:10:06 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+# Import necessary modules
+import google.generativeai as genai
+import streamlit as st
+from groq import Groq
+
+# Implement API Key 
+client = Groq(
+    api_key="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",
+)
+
+st.title("Python Trainer")
+# Declaring the prompt 
+prompt= st.text_input("Ask me anything:")
+# Create a submit button
+submit_button = st.button("Submit")
+# Contextualizing the prompt
+context_prompt = f"Act as an experienced technical trainer, who focuses in python and machine learning, give proper definitions, points to remember, code examples, summary, five practice questions and 10 interview questions on the topic  {prompt}"
+
+# Creating Model and Testing Prompt
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": context_prompt,
+        }
+    ],
+    model="llama3-70b-8192",
+)
+
+# If the submit button is clicked, display the chatbot's response
+if submit_button:
+    st.write(chat_completion.choices[0].message.content)
